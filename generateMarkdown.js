95174210f3dc2fdd1dd212cb1490b27d3ae3dd3f
suzakijun1 <dyong1997@gmail.com>
2023-04-19 13:22:23 -0400

    more more

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseOption = `${data.license}`;
+  if (licenseOption === "Apache") {
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseOption = `${data.license}`;
+  if (licenseOption === "Apache") {
+    licenseLink = "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+//if this doesnt work, change "data" to "usage, credit, license, etc"
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${description}
+
+  ##Installation
+  ${installation}
+
+  ##Usage
+  ${usage}
+
+  #Credits
+  ${credit}
+
+  ##License
+  ${license}
+
+  ##Badges
 `;
 }
 
