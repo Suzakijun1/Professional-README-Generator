db463047fe2ce651cb44ffeb0806a374fb60a74d
suzakijun1 <dyong1997@gmail.com>
2023-04-20 09:47:58 -0400

    badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseOption = `${data.license}`;
-  if (licenseOption === "Apache") {
+  if (value === "Apache"){
+    return "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (value === "MIT"){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (value === "BSD") {
+    return "[![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/license/bsd-3-clause/)";
+  } else if (value === "Unlicense"){
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  }
   }
 }
 
